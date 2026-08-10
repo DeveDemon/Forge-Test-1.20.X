547a7d2d0d70a4c52a2cc06be9e44a69f83164e6
Gleb Kravchuk <gvkravchuk@edu.hse.ru>
2026-08-11 02:15:42 +0300

    Added item with custom 3d model - sapphire_staff. Added RU and EN_US translation.

--- src/main/java/net/devedemon/testmod_forge/item/ModCreativeModTabs.java
+++ src/main/java/net/devedemon/testmod_forge/item/ModCreativeModTabs.java
@@ -36,6 +36,7 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.SAPPHIRE_DOOR.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_TRAPDOOR.get());
                         pOutput.accept(ModItems.STRAWBERRY.get());
+                        pOutput.accept(ModItems.SAPPHIRE_STAFF.get());
                         pOutput.accept(ModItems.PINE_CONE.get());
                     })
                     .build());
